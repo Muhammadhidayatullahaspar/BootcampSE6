5c1cb85cff37462f6d56423063d001067e43a9c7
Muhammad Hidayatullah Aspar <100209360+Muhammadhidayatullahaspar@users.noreply.github.com>
2023-10-12 03:15:36 +0800

    Delete Project1_Readonly directory

--- Project1_Readonly/Glass.cs
+++
@@ -1,7 +0,0 @@
-class Glass {
-	public readonly string price; // read only, jadi tidak bisa mengubah nilai price
-	public Glass(string price) {
-		this.price = price;
-	}
-
-}
--- Project1_Readonly/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿class Program {
-	static void Main(){
-		Glass gelas = new Glass("Mahal"); // construt
-		Console.WriteLine(gelas.price); // keluarkan kategori harga gelas
-		//gelas.price = "murah"; kalau diaktifkan, maka akan error karena hanya bisa di read dan tidak bisa di write ulang
-	}
-}
