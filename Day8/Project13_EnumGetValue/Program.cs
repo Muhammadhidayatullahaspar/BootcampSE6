0f0e6f6bc3217d95e6541eca0987cbcfcd287150
Muhammad Hidayatullah Aspar <100209360+Muhammadhidayatullahaspar@users.noreply.github.com>
2023-10-23 09:48:03 +0700

    Add files via upload

---
+++ Day8/Project13_EnumGetValue/Program.cs
@@ -0,0 +1,21 @@
+﻿using Project13_EnumGetValue;
+class Program
+{
+	static void Main()
+	{
+		//Enum -> int
+		int olahraga = (int)Sport.Football;
+		Console.WriteLine(olahraga); // hasilnya 948
+
+		//int -> enum (undertemine)
+		Sport sport = (Sport)732;
+		Console.WriteLine(sport); //hasilnya Basketball
+	
+		Sport[] status = (Sport[])Enum.GetValues(typeof(Sport));
+		var status2 = Enum.GetValues(typeof(Sport));
+		Console.WriteLine(status);
+		Sport[] newstatus = {Sport.Football, Sport.Basketball, Sport.Badminton};
+		int x = status.Length;
+		Console.WriteLine(x); // hasilnya 3
+	}
+}
---
+++ Day8/Project13_EnumGetValue/Sport.cs
@@ -0,0 +1,8 @@
+namespace Project13_EnumGetValue;
+
+public enum Sport
+{
+	Badminton = 383,
+	Football = 948,
+	Basketball = 732
+}
