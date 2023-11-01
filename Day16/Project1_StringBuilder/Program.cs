f4a3a36056af5ae239ad8d31adf26aba6ff55d60
Muhammad Hidayatullah Aspar <100209360+Muhammadhidayatullahaspar@users.noreply.github.com>
2023-11-01 16:36:22 +0700

    Add files via upload

---
+++ Day16/Project1_StringBuilder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+class Program
+{
+	static void Main()
+	{
+		StringBuilder sb = new StringBuilder();
+		int ukuran = 10000;
+		for (int i = 0; i < ukuran;i++)
+		{
+			sb.Append("a");
+			sb.Append("c");
+		}
+		Console.WriteLine(sb.ToString());
+		
+	}
+}
---
+++ Day16/Project1_TryGarbageCollection/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+class Program
+{
+	static void Main()
+	{
+		string nama = String.Empty;
+		int ukuran = 1000000000;
+		for (int i = 0; i < ukuran;i++)
+		{
+			nama += "a";
+			nama += "b";
+		}
+		Console.WriteLine(nama);
+		
+	}
+}
