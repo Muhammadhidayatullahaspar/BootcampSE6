5c9238edb8c1884d564ef5b51a0e7df486e49d6b
Muhammad Hidayatullah Aspar <100209360+Muhammadhidayatullahaspar@users.noreply.github.com>
2023-11-06 15:28:40 +0700

    Add files via upload

---
+++ Day18/Project7_ThreadName/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+class Program
+{
+	static void Main()
+	{
+		Thread thread1 = new Thread(Fruit1);
+		Thread thread2 = new Thread(Fruit2);
+		thread1.Name = "Fresh Fruit";
+		thread2.Name = "Expired fruit";
+		
+		thread2.Start(); 
+		thread1.Start();
+		
+		thread1.Join();
+		thread2.Join();
+
+		Console.WriteLine("thread utama telah berakhir");
+	}
+	static void Fruit1()
+	{
+		Console.WriteLine("thread yang menjalankan buah segar telah berjalan");
+		Thread.Sleep(20000);
+		Console.WriteLine("thread buah segar telah selesai dijalankan");
+	}
+	static void Fruit2()
+	{
+		Console.WriteLine("thread yang menjalankan buah tidak segar telah berjalan");
+		Thread.Sleep(30000);
+		Console.WriteLine("thread buah tidak segar telah selesai dijalankan");
+	}
+}
---
+++ Day18/Project8_Priority/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Thread t1 = new Thread(() =>
+        {
+            Console.WriteLine("Thread 1 started");
+            Thread.Sleep(5000);
+            Console.WriteLine("Thread 1 finished");
+        });
+        t1.Priority = ThreadPriority.Lowest;
+        
+        Thread t2 = new Thread(() =>
+        {
+            Console.WriteLine("Thread 2 started");
+            Thread.Sleep(5000);
+            Console.WriteLine("Thread 2 finished");
+        });
+        t2.Priority = ThreadPriority.Highest;
+        
+        // Start the threads
+        t1.Start();
+        t2.Start();
+        
+        // Wait for the threads to finish
+        t1.Join();
+        t2.Join();
+        
+        Console.WriteLine("All threads finished");
+    }
+}
