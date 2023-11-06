e9463b32a8e85fd84871911c4b85b386876879ca
Muhammad Hidayatullah Aspar <100209360+Muhammadhidayatullahaspar@users.noreply.github.com>
2023-11-06 17:01:14 +0700

    Add files via upload

---
+++ Day19/Project4_TaskContinueWait/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+class Program
+{
+	static void Main()
+	{
+		Task task = Task.Run(() => Console.WriteLine("Hello from Task 1!"));
+		Task continuation = task.ContinueWith(x => Console.WriteLine("Hello from Task 2!"));
+		continuation.Wait();
+	}
+}
---
+++ Day19/Project5_TaskWhenAll/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+	static void Main()
+	{
+		Task task1 = Task.Run(() => Console.WriteLine("Hello from Task 1!"));
+		Task task2 = Task.Run(() => Console.WriteLine("Hello from Task 2!"));
+		
+		Task.WhenAll(task1, task2);
+		
+		Console.WriteLine("Hello from Task 3!");
+	}
+}
