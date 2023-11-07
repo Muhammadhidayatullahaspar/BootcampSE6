ed54f2755003884d583e7a978a895b6d2aca9643
Muhammad Hidayatullah Aspar <100209360+Muhammadhidayatullahaspar@users.noreply.github.com>
2023-11-07 16:46:36 +0700

    Add files via upload

---
+++ Day20/Project3_ConditionalCompilation/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+	static void Main()
+	{
+		#if HOROR
+		Console.WriteLine("ini film horor");
+		#elif COMEDY
+		Console.WriteLine("ini film comedy");
+		#endif
+		Console.WriteLine("selesai");
+	}
+}
---
+++ Day20/Project4_Debugger/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+	
+	static void Main()
+	{
+		double a = 5;
+		double b = 10;
+		double c = Divide(a,b);
+		Console.WriteLine($"hasil dari pembagiaannya adalah {c}");
+	}
+	/// <summary>
+	/// ini untuk membagi bilangan
+	/// </summary>
+	/// <param name="a untuk bilangan pertama"></param>
+	/// <param name="b untuk bilangan kedua"></param>
+	/// <returns></returns>
+	static double Divide(double a, double b)
+	{
+		return a + b;
+	}
+}
