4d36928220eda75af0562f79806e91d916b557d6
Muhammad Hidayatullah Aspar <100209360+Muhammadhidayatullahaspar@users.noreply.github.com>
2023-11-07 16:53:39 +0700

    Add files via upload

---
+++ Day20/Project8_ConsoleTraceListener/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace MyNamespace
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ConsoleTraceListener consoleTraceListener = new ConsoleTraceListener();
+			Trace.Listeners.Add(consoleTraceListener);
+
+			Trace.WriteLine("Starting program");
+
+			int x = 5;
+			int y = 10;
+
+			Trace.WriteLine($"x = {x}, y = {y}");
+
+			int sum = x + y;
+
+			Trace.WriteLine($"The sum of {x} and {y} is {sum}");
+
+			Trace.WriteLine("Program complete");
+		}
+	}
+}
