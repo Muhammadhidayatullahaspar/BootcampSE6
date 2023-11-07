3ca91ee5d4effe6336298158aa020489fbf336e3
Muhammad Hidayatullah Aspar <100209360+Muhammadhidayatullahaspar@users.noreply.github.com>
2023-11-07 16:43:34 +0700

    Add files via upload

---
+++ Day20/Project1_ConditionalCompilation/Program.cs
@@ -0,0 +1,15 @@
+﻿#define Horor
+class Program
+{
+	static void Main()
+	{
+		#if Comedy
+		Console.WriteLine("ini buat film genre comedy");
+		#elif Horor
+		Console.WriteLine("ini buat film genre Horor");
+		#else
+		Console.WriteLine("tidak ada genre film yang dipilih");
+		#endif
+		Console.WriteLine("selesai");
+	}
+}
---
+++ Day20/Project2_ConditionalCompilation2/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+	static void Main()
+	{
+		#if COMEDY
+		Console.WriteLine("ini film comedy");
+		#elif DEBUG
+		Console.WriteLine("ini film horor");
+		#endif
+		Console.WriteLine("selesai");
+	}
+}
