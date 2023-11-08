8b1e78c5297a6b678bc295e4f2d922b1e732c9c6
Muhammad Hidayatullah Aspar <100209360+Muhammadhidayatullahaspar@users.noreply.github.com>
2023-11-08 16:28:52 +0700

    Add files via upload

---
+++ Day21/Project7_StreamWriterUsing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+namespace FileHandlinDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"MyFile.txt";
+
+          
+            int a, b, result;
+            a = 15;
+            b = 20;
+            result = a + b;
+
+          
+            using (StreamWriter streamWriter = new StreamWriter(filePath, true))
+            {
+                streamWriter.Write($"Sum of {a} + {b} = {result}");
+            }
+            Console.WriteLine("Variable Data is Saved into the File");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day21/Project8_StreamReader/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+namespace FileHandlinDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string filePath = @"MyFile.txt";       
+            StreamReader streamReader = new StreamReader(filePath);
+            Console.WriteLine("Content of the File");
+
+            streamReader.BaseStream.Seek(0, SeekOrigin.Begin);
+            string strData = streamReader.ReadLine();
+            while (strData != null)
+            {
+                Console.WriteLine(strData);
+                strData = streamReader.ReadLine();
+            }
+            Console.ReadLine();
+            streamReader.Close();
+            Console.ReadKey();
+        }
+    }
+}
