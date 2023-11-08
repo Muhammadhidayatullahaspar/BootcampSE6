0dbfbd342d8586a2725e143776a09d8a4c18de7f
Muhammad Hidayatullah Aspar <100209360+Muhammadhidayatullahaspar@users.noreply.github.com>
2023-11-08 16:41:34 +0700

    Add files via upload

---
+++ Day21/Project9_StreamReaderReadToEnd/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+namespace FileHandlinDemo
+{
+	class Program
+	{
+		/// <summary>
+		/// buat baca file txt lalu munculkan isinya pada terminal
+		/// </summary>
+		/// <param name="args"></param>
+		static void Main(string[] args)
+		{
+			string filePath = @"MyFile.txt";
+
+			StreamReader streamReader = new StreamReader(filePath);
+
+			Console.WriteLine("Content of the File");
+			
+			using (StreamReader reader = new StreamReader(filePath))
+			{
+				Console.WriteLine(reader.ReadToEnd());
+			}
+			
+			Console.ReadKey();
+		}
+	}
+}
