dc299a6271ee1b23233c73ad7862cd66be79edab
Muhammad Hidayatullah Aspar <100209360+Muhammadhidayatullahaspar@users.noreply.github.com>
2023-11-15 17:00:02 +0700

    Add files via upload

---
+++ Day26/Project10_BeforeDIP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class EmailNotifier
+{
+    public void Notify(string message)
+    {
+        Console.WriteLine("Sending email notification: " + message);
+    }
+}
+
+public class UserRegistration
+{
+    private EmailNotifier _emailNotifier;
+
+    public UserRegistration()
+    {
+        _emailNotifier = new EmailNotifier();
+    }
+
+    public void Register(string userName)
+    {
+        Console.WriteLine("Registering user: " + userName);
+        _emailNotifier.Notify("User " + userName + " registered.");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var userRegistration = new UserRegistration();
+        userRegistration.Register("John");
+    }
+}
---
+++ Day26/Project11_AfterDIP/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public interface INotifier
+{
+    void Notify(string message);
+}
+
+public class EmailNotifier : INotifier
+{
+    public void Notify(string message)
+    {
+        Console.WriteLine("Sending email notification: " + message);
+    }
+}
+
+public class UserRegistration
+{
+    private INotifier _notifier;
+
+    public UserRegistration(INotifier notifier)
+    {
+        _notifier = notifier;
+    }
+
+    public void Register(string userName)
+    {
+        Console.WriteLine("Registering user: " + userName);
+        _notifier.Notify("User " + userName + " registered.");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        INotifier emailNotifier = new EmailNotifier();
+        var userRegistration = new UserRegistration(emailNotifier);
+        userRegistration.Register("John");
+    }
+}
