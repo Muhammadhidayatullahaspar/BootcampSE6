5fb009527cd1ca314c1b482b239343a800be05df
Muhammad Hidayatullah Aspar <100209360+Muhammadhidayatullahaspar@users.noreply.github.com>
2023-11-16 16:50:05 +0700

    Add files via upload

---
+++ Day27/Calculation/Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Calculation 
+{
+    internal class Program 
+	{
+		static void Main(string[] args) 
+		{
+			NumberCalculation numberCalc = new();
+			int a = 10;
+			int b = 12;
+
+			Console.WriteLine($"Addition of {a} and {b} is {numberCalc.Add(a, b)}");
+			Console.WriteLine($"Subtraction of {a} and {b} is {numberCalc.Subtract(a, b)}");
+			Console.WriteLine($"Multiplication of {a} and {b} is {numberCalc.Multiply(a, b)}");
+			Console.WriteLine($"Division of {a} and {b} is {numberCalc.Divide(a, b)}");
+		}
+	}
+	
+	public class NumberCalculation
+	{
+		public int Add(int a, int b) 
+		{
+			return a + b;
+		}
+		public int Multiply(int a, int b) 
+		{
+			return a * b;
+		}
+		public int Subtract(int a, int b) 
+		{
+			return a - b;
+		}
+		public int Divide(int a, int b) 
+		{
+			return a / b;
+		}
+	}
+}
