8b1aa6bb913a8c56e913421a4a4dd943fdafaf3e
Yayat <muhammadhidayatullahaspar@gmail.com>
2023-11-25 01:29:26 +0800

    ganjilgenap

---
+++ Practice/Practice1/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("silahkan masukkan nilai");
+		int num = Convert.ToInt32(Console.ReadLine());
+		Number number = new Number();
+		number.setNumber(num);
+		
+		
+	}
+}
+public class Number
+{
+	
+	public void setNumber(int num)
+	{
+		if (num % 2 == 0)
+		{
+			Console.WriteLine("bilangan genap");
+		}
+		else
+		{
+			Console.WriteLine("bilangan ganjil");
+		}
+	}
+}
