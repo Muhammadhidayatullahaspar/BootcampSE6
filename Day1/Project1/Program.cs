9f6f044fbe0b04620e61a58ebd2adfd8dfdda47b
Yayat <muhammadhidayatullahaspar@gmail.com>
2023-11-25 00:22:52 +0800

    update

--- Day1/Project1/Program.cs
+++ Day1/Project1/Program.cs
@@ -2,7 +2,7 @@
 {
 	static void Main()
 	{
-		Cat Oyen = new Cat(); // construct kucing baru
+		Cat Oyen = new Cat(); // instance of Cat
 		Oyen.colour = "Orange"; // oren diberi warna orange
 		Console.WriteLine(Oyen.colour); // cetak
 		
