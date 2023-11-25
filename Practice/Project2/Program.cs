63637fe3da1d2446ff6607aa5b57afaf1ce85184
Yayat <muhammadhidayatullahaspar@gmail.com>
2023-11-25 21:13:43 +0800

    StringReverse

---
+++ Practice/Project2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("please input your words...");
+		string input = Console.ReadLine();
+		Words words = new();
+		string output = words.Reverse(input);
+		Console.WriteLine(output);
+		
+	}
+}
+
+public class Words
+{
+	public string Reverse (string input)
+	{
+		string output = "";
+		for (int i = input.Length -1 ; i >=0; i--)
+		{
+			output += input[i];
+		}
+		return output;
+	}
+}
