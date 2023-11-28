fdce876c3a1f34a18e0fe79bcb7acfe7d272825b
Yayat <muhammadhidayatullahaspar@gmail.com>
2023-11-28 09:24:44 +0800

    Sum

---
+++ Practice/Project3/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+	static void Main()
+	{
+		Calc calc = new Calc();
+		int output = calc.Sum(5);
+		Console.WriteLine(output);
+	}
+}
+public class Calc
+{
+	public int Sum(int number)
+	{
+		number += 3;
+		return number;
+	}
+}
